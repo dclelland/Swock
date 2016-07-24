1c637709b352c8e818ff618dbb80c7956f78f32a
Daniel Clelland <daniel.clelland@gmail.com>
2016-07-24 19:23:31 +1200

    Added comments

--- Swock/Swock.swift
+++ Swock/Swock.swift
@@ -87,41 +87,80 @@ public func tar(noun: Noun) -> Noun {
     switch noun {
     case let .Cell(a, formula):
         switch formula {
+            
         // If the head of the formula is a cell, Nock treats both head and tail as formulas, resolves each against the subject, and produces the cell of their products.
+        // *[a [b c] d] -> [*[a b c] *[a d]]
+            
         case let .Cell(.Cell(b, c), d):
             return .Cell(tar([a, b, c]), tar([a, d]))
+            
         // A formula [0 b] produces the noun at tree address b in the subject.
+        // *[a 0 b] -> /[b a]
+            
         case let .Cell(0, b):
             return fas([b, a])
+            
         // A formula [1 b] produces the constant noun b.
+        // *[a 1 b] -> b
+            
         case let .Cell(1, b):
             return b
+            
         // A formula [2 b c] treats b and c as formulas, resolves each against the subject, then computes Nock again with the product of b as the subject, c as the formula.
+        // *[a 2 b c] -> *[*[a b] *[a c]]
+            
         case let .Cell(2, .Cell(b, c)):
             return tar([tar([a, b]), tar([a, c])])
-        // In formulas [3 b], [4 b], and [5 b], b is another formula, whose product against the subject becomes the input to an axiomatic operator. 3 is ?, 4 is +, 5 is =.
+            
+        // A formula [3 b], [4 b], and [5 b], b is another formula, whose product against the subject becomes the input to an axiomatic operator. 3 is ?, 4 is +, 5 is =.
+        // *[a 3 b] -> ?*[a b]
+            
         case let .Cell(3, b):
             return wut(tar([a, b]))
+            
+        // *[a 4 b] -> +*[a b]
+            
         case let .Cell(4, b):
             return lus(tar([a, b]))
+            
+        // *[a 5 b] -> =*[a b]
+            
         case let .Cell(5, b):
             return tis(tar([a, b]))
+            
         // [6 b c d] is if b, then c, else d. Each of b, c, d is a formula against the subject. Remember that 0 is true and 1 is false.
+        // *[a 6 b c d] -> *[a 2 [0 1] 2 [1 c d] [1 0] 2 [1 2 3] [1 0] 4 4 b]
+            
         case let .Cell(6, .Cell(b, .Cell(c, d))):
             return tar([a, 2, [0, 1], 2, [1, c, d], [1, 0], 2, [1, 2, 3], [1, 0], 4, 4, b])
+            
         // [7 b c] composes the formulas b and c.
+        // *[a 7 b c] -> *[a 2 b 1 c]
+            
         case let .Cell(7, .Cell(b, c)):
             return tar([a, 2, b, 1, c])
+            
         // [8 b c] produces the product of formula c, against a subject whose head is the product of formula b with the original subject, and whose tail is the original subject. (Think of 8 as a "variable declaration" or "stack push.")
+        // *[a 8 b c] -> *[a 7 [[7 [0 1] b] 0 1] c]
+            
         case let .Cell(8, .Cell(b, c)):
             return tar([a, 7, [[7, [0, 1], b], 0, 1], c])
+            
         // [9 b c] computes the product of formula c with the current subject; from that product d it extracts a formula e at tree address b, then computes *[d e]. (9 is designed to fit Hoon; d is a core (object), e points to an arm (method).)
+        // *[a 9 b c] -> *[a 7 c 2 [0 1] 0 b]
+            
         case let .Cell(9, .Cell(b, c)):
             return tar([a, 7, c, 2, [0, 1], 0, b])
+            
         // [10 b c] is a hint semantically equivalent to the formula c. If b is an atom, it's a static hint, which is just discarded. If b is a cell, it's a dynamic hint; the head of b is discarded, and the tail of b is executed as a formula against the current subject; the product of this is discarded.
-        case let .Cell(10, .Cell(.Cell(_, c), d)): // Dynamic hint
+        // *[a 10 [b c] d] -> *[a 8 c 7 [0 3] d]
+            
+        case let .Cell(10, .Cell(.Cell(_, c), d)):
             return tar([a, 8, c, 7, [0, 3], d])
-        case let .Cell(10, .Cell(_, c)): // Static hint
+            
+        // *[a 10 b c] -> *[a c]
+            
+        case let .Cell(10, .Cell(_, c)):
             return tar([a, c]);
         default:
             fatalError("Invalid `tar()` formula")
