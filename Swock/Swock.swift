0c74b70f572267991a49ce9e1a2095ffdd9eb6cb
Daniel Clelland <daniel.clelland@gmail.com>
2016-07-24 17:36:42 +1200

    Got autocons working

---
+++ Swock/Swock.swift
@@ -0,0 +1,69 @@
+//
+//  Swock.swift
+//  Swock
+//
+//  Created by Daniel Clelland on 24/07/16.
+//  Copyright © 2016 Daniel Clelland. All rights reserved.
+//
+
+import Foundation
+
+indirect enum Noun {
+    
+    case Atom(UInt)
+    
+    case Cell(Noun, Noun)
+    
+    init(_ elements: [Noun]) {
+        switch (elements.head, elements.tail) {
+        case (let head?, nil):
+            self = head
+        case (let head?, let tail?):
+            self = .Cell(head, Noun(tail))
+        default:
+            fatalError("Empty noun")
+        }
+    }
+    
+}
+
+extension Noun: IntegerLiteralConvertible {
+    
+    init(integerLiteral value: UInt) {
+        self = .Atom(value)
+    }
+
+}
+
+extension Noun: ArrayLiteralConvertible {
+    
+    init(arrayLiteral elements: Noun...) {
+        self.init(elements)
+    }
+    
+}
+
+extension Noun: CustomStringConvertible {
+    
+    var description: String {
+        switch self {
+        case .Atom(let value):
+            return String(value)
+        case .Cell(let head, let tail):
+            return "[\(head.description) \(tail.description)]"
+        }
+    }
+    
+}
+
+private extension Array {
+    
+    var head: Element? {
+        return count > 0 ? self[0] : nil
+    }
+    
+    var tail: [Element]? {
+        return count > 1 ? Array(self[1..<count]) : nil
+    }
+    
+}
---
+++ Swock/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swock
+//
+//  Created by Daniel Clelland on 24/07/16.
+//  Copyright © 2016 Daniel Clelland. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let noun: Noun = [12, 34, 56, 78]
+
+print(noun)
